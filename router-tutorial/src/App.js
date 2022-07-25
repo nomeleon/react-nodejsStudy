1ece1ed8dde14d3a1205a21432b8056cda704ee2
노소연 <92971204+soyeonie@users.noreply.github.com>
2022-07-25 09:28:37 +0900

    router

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+//yarn add react-router
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  );
+};
+
+export default App;
