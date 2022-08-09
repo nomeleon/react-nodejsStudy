5f8db6972b2ca73e23a965055da408ad602aae7d
노소연 <92971204+soyeonie@users.noreply.github.com>
2022-08-09 11:54:00 +0900

    express-mysql-session multer

--- learn-express/app.js
+++ learn-express/app.js
@@ -5,6 +5,17 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
+const MySQLStore = require("express-mysql-session")(session);
+
+const options = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "123456",
+  database: "bbs",
+};
+
+var sessionStore = new MySQLStore(options);
 
 dotenv.config();
 //웹 서버 생성
@@ -22,6 +33,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
+    store: sessionStore,
     cookie: {
       httpOnly: true,
       secure: false,
@@ -29,10 +41,38 @@ app.use(
     name: "session-cookie",
   })
 );
-// app.use((req, res, next) => {
-//   console.log("모든 요청에 다 실행됩니다.");
-//   next();
-// });
+
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+});
+
+app.use((req, res, next) => {
+  console.log("모든 요청에 다 실행됩니다.");
+  sess = req.session;
+  sess.username = "soyeon";
+  console.log(req.session.username);
+  console.log(req.sessionID);
+  console.log(req.session);
+  next();
+});
 //실제 수행
 app.get(
   "/",
